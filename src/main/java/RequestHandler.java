de2ed7816b604ecee5c3b28838a5c75c1ded552f
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-11-23 13:43:18 +0530

    expire flag added

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -11,7 +11,7 @@ public class RequestHandler {
 
     private Socket clientSocket;
 
-    static ConcurrentHashMap<String, String> hashMap = new ConcurrentHashMap<>();
+    static ConcurrentHashMap<String, HashMapValue> hashMap = new ConcurrentHashMap<>();
 
     public RequestHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -36,7 +36,7 @@ public class RequestHandler {
                             continue;
                         }
                         int length = Integer.parseInt(lengthLine.substring(1));
-                        args[i] = reader.readLine(); 
+                        args[i] = reader.readLine();
                         if (args[i].length() != length) {
                             writer.write("-ERROR: Length mismatch\r\n");
                             writer.flush();
@@ -44,30 +44,50 @@ public class RequestHandler {
                         }
                     }
                     System.out.println(Arrays.toString(args));
+
                     if (args[0].equalsIgnoreCase("ping")) {
                         writer.write("+PONG\r\n");
                         writer.flush();
-                    }else if (args[0].equalsIgnoreCase("set") && numArgs == 3) {
-                        hashMap.put(args[1], args[2]);
+
+                    } else if (args[0].equalsIgnoreCase("set") && numArgs >= 3) {
+                        HashMapValue hashMapValue = new HashMapValue();
+                        hashMapValue.value = args[2];
+                        if (numArgs > 3) {
+                            if (args[3].equalsIgnoreCase("px")) {
+                                hashMapValue.expiry = System.currentTimeMillis() + Long.parseLong(args[4]);
+                            }
+                        } else {
+                            hashMapValue.expiry = -1;
+                        }
+                        hashMap.put(args[1], hashMapValue);
                         writer.write("+OK\r\n");
                         writer.flush();
-                    }else if (args[0].equalsIgnoreCase("get") && numArgs == 2) {
-                        if(hashMap.containsKey(args[1])){
-                            String value = hashMap.get(args[1]);
-                            writer.write("$" + value.length() + "\r\n" + value + "\r\n");
-                            writer.flush();
-                        }else{
+
+                    } else if (args[0].equalsIgnoreCase("get") && numArgs == 2) {
+                        if (hashMap.containsKey(args[1])) {
+                            HashMapValue obj = hashMap.get(args[1]);
+                            if (System.currentTimeMillis() < obj.expiry) {
+                                writer.write("$" + obj.value.length() + "\r\n" + obj.value + "\r\n");
+                                writer.flush();
+                            } else {
+                                writer.write("$-1\r\n");
+                                writer.flush();
+                            }
+                        } else {
                             writer.write("-ERROR: Unknown key\r\n");
-                            writer.flush();    
+                            writer.flush();
                         }
-                    }else if (args[0].equalsIgnoreCase("echo") && numArgs == 2) {
+
+                    } else if (args[0].equalsIgnoreCase("echo") && numArgs == 2) {
                         String message = args[1];
                         writer.write("$" + message.length() + "\r\n" + message + "\r\n");
                         writer.flush();
+
                     } else {
                         writer.write("-ERROR: Unknown command or incorrect arguments\r\n");
                         writer.flush();
                     }
+
                 } else {
                     writer.write("-ERROR: Invalid RESP input\r\n");
                     writer.flush();
@@ -75,7 +95,7 @@ public class RequestHandler {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             try {
                 clientSocket.close(); // Ensure socket is closed to avoid resource leaks
             } catch (IOException e) {
@@ -83,4 +103,9 @@ public class RequestHandler {
             }
         }
     }
+}
+
+class HashMapValue {
+    String value;
+    long expiry;
 }
