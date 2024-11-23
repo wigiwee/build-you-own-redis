843fca1dcb136482b10aa85d9522034619616b45
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-11-23 13:06:26 +0530

    echo command implemented 4

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -58,7 +58,7 @@ public class RequestHandler {
                     writer.flush();
                 }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }finally{
             try {
