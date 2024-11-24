964b05202ebfda445ec6cb556d8fcf44f9758056
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-11-24 17:23:30 +0530

    reading timestamp from rdb file

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -117,6 +117,7 @@ public class RequestHandler {
                         writer.flush();
 
                     } else if (args[0].equalsIgnoreCase("get") && numArgs == 2) {
+                        System.out.println(Arrays.asList(RdbFile.getKeys()));
                         RdbFile.refreshRDBFile();
                         if (keyValueHashMap.containsKey(args[1])) {
                             if (keyExpiryHashMap.containsKey(args[1])) {
