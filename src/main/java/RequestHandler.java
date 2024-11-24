4b4dd6464e7c0309db892825530edb5a737889ae
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-11-25 01:58:21 +0530

    sending empty rdb file to slave

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -177,9 +177,9 @@ public class RequestHandler {
                         StringBuilder output = new StringBuilder();
                         output.append("$").append(tempFile.length);
                         output.append(Config.CRLF);
-                        for (byte b : tempFile) {
-                        output.append(b);
-                        }
+                        // for (byte b : tempFile) {
+                        // output.append(b);
+                        // }
                         writer.write(output.toString());
                         writer.flush();
                         // String file = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
