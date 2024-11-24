d138eac54fafd1f9258c1ecfda5376d1276b0d8f
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-11-24 18:49:42 +0530

    debugging

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -75,7 +75,6 @@ public class RequestHandler {
 
     public void run() throws IOException {
         RdbFile.refreshRDBFile();
-        System.out.println(Arrays.toString(RdbFile.getKeys()));
         try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));) {
